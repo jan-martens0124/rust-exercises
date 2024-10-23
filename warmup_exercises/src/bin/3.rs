dba4abd0f470ed305e4badbb401fe863cea2cd09
jan-martens0124 <135309102+jan-martens0124@users.noreply.github.com>
2024-10-23 21:41:43 +0200

    warmup oef3

--- warmup_exercises/src/bin/3.rs
+++ warmup_exercises/src/bin/3.rs
@@ -3,7 +3,7 @@
 fn main() {
     let mut s0 = String::new();
     
-    append_to_string(s0);
+    s0 = append_to_string(s0);
 
     println!("{}",  s0);
 
@@ -13,8 +13,28 @@ fn main() {
 }
 
 //keep this function
-fn append_to_string(s: String) {
+fn append_to_string(mut s: String) -> String {
     s.push_str("Hello");
     s.push_str(" ");
     s.push_str("World");
+    s
 }
+
+// fn main() {
+//     let mut s0 = String::new();
+    
+//     append_to_string(&mut s0);
+
+//     println!("{}",  s0);
+
+//     s0.push_str("!");
+
+//     println!("{}", s0);
+// }
+
+// //keep this function
+// fn append_to_string(s: &mut String) {
+//     s.push_str("Hello");
+//     s.push_str(" ");
+//     s.push_str("World");
+// }
